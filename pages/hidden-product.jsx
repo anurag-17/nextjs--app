bd5e28dc58939c7617612e8c083d27beb82e4aa3
Anurag Pandey <anurag.pandey17@gmail.com>
2023-10-31 17:53:10 +0530

    new-create

---
+++ pages/hidden-product.jsx
@@ -0,0 +1,51 @@
+
+import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+
+const HiddenProduct = () => {
+  const [allProduct, setAllProduct] = useState([]);
+
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+
+  const getAllProducts = async () => {
+    const options = {
+      method: "GET",
+      url: "https://e-commerce-backend-brown.vercel.app/api/product/getAllProduct",
+      headers: {
+        cookie:
+          "refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MWQ5MzJjZDk3NGZlZjA3YWQzMmNkZSIsImlhdCI6MTY5NjQ4OTg5MiwiZXhwIjoxNjk2NzQ5MDkyfQ.r9M7MHA5dLHqKU0effObV0mwYE60SCEUt2sfiWUZzEw",
+        "User-Agent": "insomnia/2023.5.8",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        if (response.status === 200) {
+          setAllProduct(response?.data);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+
+  return (
+    <>
+    <div>
+    
+
+    </div>
+    </>
+  )
+}
+
+export default HiddenProduct
