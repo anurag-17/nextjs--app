c321e2ec384a06833e1f5921c61f104a0663c768
Anurag Pandey <anurag.pandey17@gmail.com>
2023-11-08 19:45:43 +0530

    update

--- utlis/api.js
+++ utlis/api.js
@@ -1,41 +1,36 @@
 // utils/api.js
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-const BASE_URL = 'https://e-commerce-backend-brown.vercel.app/api'; 
+const BASE_URL = "https://e-commerce-backend-brown.vercel.app/api";
 
 export const fetchApi = async (url, options = {}) => {
   try {
+    const accessToken = JSON.parse(localStorage.getItem("userToken"));
+    // const token = useSelector((state) => state);
 
-  const accessToken = JSON.parse(localStorage.getItem("userToken")
-  )
-  // const token = useSelector((state) => state);
-  
-  const headers = {
-    'Content-Type': 'application/json',
-    'User-Agent': 'PostmanRuntime/7.33.0',
-  };
-  
-  if (accessToken) {
-      headers['authorization'] = accessToken ;
+    const headers = {
+      "Content-Type": "application/json",
+      "User-Agent": "PostmanRuntime/7.33.0",
+    };
+
+    if (accessToken) {
+      headers["authorization"] = accessToken;
     }
 
-
     const response = await fetch(`${BASE_URL}${url}`, {
-      method: 'GET', // Change the method as needed (e.g., 'POST', 'PUT', etc.)
+      method: "GET", // Change the method as needed (e.g., 'POST', 'PUT', etc.)
       headers,
       ...options,
     });
-    
-    
+
     // if (!response.ok) {
     //   throw new Error(`API request failed with status ${response.status}`);
     // }
-    
+
     // const data = await response.json();
 
     return response;
   } catch (error) {
     throw error;
-    
   }
 };
